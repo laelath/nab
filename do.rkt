e6f9c9502f31d6566d5d6a2c4462385b2e0598a5
Pierce Darragh <pierce.darragh@gmail.com>
2022-05-09 15:42:01 -0400

    curse the interpreter with bastardized monadic do-notation

---
+++ do.rkt
@@ -0,0 +1,177 @@
+#lang racket
+
+(require "env.rkt"
+         "sto.rkt")
+(require (for-syntax syntax/parse
+                     racket/match))
+(provide (all-defined-out))
+
+;; The do-block creates a code block where an environment and store are threaded
+;; along throughout the program, similar to a state-carrying monadic do
+;; environment. It's a bit cursed, but it makes implementations involving the
+;; environment and store a lot easier to write.
+;;
+;; The do-block requires three parameters:
+;;
+;;     #:error-value errv          This is an error-propagating state monad, so
+;;                                     this parameter describes the kind of
+;;                                     errors we are using. Some examples might
+;;                                     be ['err] or [#f].
+;;
+;;     #:env r                     The current environment.
+;;
+;;     #:sto s                     The current store.
+;;
+;; These are the forms allowed inside the do-block:
+;;
+;; (! e) d ...+                    Executes [e] and discards the result. Then,
+;;                                     proceeds with [d ...]. (Useful for
+;;                                     printing.)
+;;
+;; (v <- e) d ...+                 Executes [e]. If the result is ['err], the
+;;                                     ['err] is returned and execution stops
+;;                                     (no additional [d ...] are evaluated). If
+;;                                     the result of [e] is not ['err], the
+;;                                     result is bound to [v] and evaluation
+;;                                     proceeds with [d ...].
+;;
+;; (p = e) d ...+                  Binds the pattern [p] to the expression [e],
+;;                                     then proceeds with [d ...].
+;;
+;; (if c e1 e2) d ...+             Evaluates the conditional expression [c]. If
+;;                                     it is true, evaluates [e1]. Otherwise,
+;;                                     evaluates [e2]. Then, proceeds with
+;;                                     [d ...].
+;;
+;; (ls := extend* xs vs) d ...+    Extends the environment and store to map the
+;;                                     variables [xs] to the values [vs], and
+;;                                     binds the new locations to [ls]. Then,
+;;                                     proceeds with [d ...].
+;;
+;; (extend* xs vs) d ...+          Extends the environment and store to map the
+;;                                     variables [xs] to the values [vs]. Then,
+;;                                     proceeds with [d ...].
+;;
+;; (l := extend x v) d ...+        Extends the environment and store to map the
+;;                                     variable [x] to the value [v], and binds
+;;                                     the new location to [l]. Then, proceeds
+;;                                     with [d ...].
+;;
+;; (extend x v) d ...+             Extends the environment and store to map the
+;;                                     variable [x] to the value [v]. Then,
+;;                                     proceeds with [d ...].
+;;
+;; (return v:id)                   Returns a pair of the identifier [v] with the
+;;                                     current store [s].
+;;
+;; (return e)                      Returns the result of evaluating [e].
+;;
+;; (return-if c v:id)              Evaluates the conditional expression [c]. If
+;;                                     it is true, returns a pair of the
+;;                                     identifier [v] with the current store
+;;                                     [s]. Otherwise, returns ['err].
+;;
+;; (return-if c e)                 Evaluates the conditional expression [c]. If
+;;                                     it is true, returns the result of
+;;                                     evaluating [e]. Otherwise returns ['err].
+;;
+;; e                               Evaluates the expression [e], returning the
+;;                                     result.
+(define-syntax (do-block stx)
+  (syntax-parse stx
+    #:datum-literals (! <- = := extend extend* return return-if)
+    ;; (! e) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (! expr) exps ...+)
+     #'(begin expr (do-block #:error-value errv #:env r #:sto s exps ...))]
+    ;; (v <- e) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (v <- expr) exps ...+)
+     #'(match expr
+         [errv errv]
+         [(cons v s)
+          (do-block #:error-value errv #:env r #:sto s exps ...)]
+         [_ errv])]
+    ;; (x = e) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (p = expr) exps ...+)
+     #'(match expr
+         [errv errv]
+         [p (do-block #:error-value errv #:env r #:sto s exps ...)]
+         [_ errv])]
+    ;; (ls := extend* xs vs) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (new-allocated-locations := extend* xs vs) exps ...+)
+     #'(let-values ([(r s ls) (for/fold ([r r] [s s] [ls '()])
+                                        ([xvs (map cons xs vs)])
+                                (match xvs
+                                  [(cons x v)
+                                   (match (extend-sto s v)
+                                     [(cons s l)
+                                      (let ([r (extend-env r x l)])
+                                        (values r s (cons l ls)))])]))])
+         (match-let ([new-allocated-locations ls])
+           (do-block #:error-value errv #:env r #:sto s exps ...)))]
+    ;; (extend* xs vs) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (extend* xs vs) exps ...+)
+     #'(do-block #:error-value errv #:env r #:sto s (_ := extend* xs vs) exps ...)]
+    ;; (l := extend x v) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (new-allocated-location := extend x v) exps ...+)
+     #'(do-block #:error-value errv #:env r #:sto s ((cons new-allocated-location _) := extend* (list x) (list v)) exps ...)]
+    ;; (extend x v) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (extend x v) exps ...+)
+     #'(do-block #:error-value errv #:env r #:sto s (extend* (list x) (list v)) exps ...)]
+    ;; (if c e1 e2) d ...+
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        ((~datum if) c e1 e2))
+     #'(if c
+           (do-block #:error-value errv #:env r #:sto s e1)
+           (do-block #:error-value errv #:env r #:sto s e2))]
+    ;; (return v:id)
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (return v:id))
+     #'(cons v s)]
+    ;; (return e)
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (return e:expr))
+     #'(cons e s)]
+    ;; (return-if c v:id)
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (return-if c v:id))
+     #'(if c (cons v s) errv)]
+    ;; (return-if c e)
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        (return-if c e:expr))
+     #'(if c
+           (do-block #:error-value errv #:env r #:sto s (return e))
+           errv)]
+    ;; e
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s) expr)
+     #'expr]))
+
+(define-syntax (match-do stx)
+  (syntax-parse stx
+    [(_ (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s)
+        expr [pat body ...+] ...+)
+     #'(match expr
+         [pat (do-block #:error-value errv #:env r #:sto s body ...)] ...)]))
+
+(define-syntax (define-match-do stx)
+  (syntax-parse stx
+    [(_ (name:id left ...+ (~seq #:error-value errv) right ...) clauses ...+)
+     #'(define-match-do (name #:error-value errv left ... right ...) clauses ...)]
+    [(_ (name:id (~seq #:error-value errv) left ...+ (~seq #:env r) right ...) clauses ...+)
+     #'(define-match-do (name #:error-value errv #:env r left ... right ...) clauses ...)]
+    [(_ (name:id (~seq #:error-value errv) (~seq #:env r) left ...+ (~seq #:sto s) right ...) clauses ...+)
+     #'(define-match-do (name #:error-value errv #:env r #:sto s left ... right ...) clauses ...)]
+    [(_ (name:id (~seq #:error-value errv) (~seq #:env r) (~seq #:sto s) left ...+ (~seq #:matching m) right ...) clauses ...+)
+     #'(define-match-do (name #:error-value errv #:env r #:sto s #:matching m left ... right ...) clauses ...)]
+    [(_ (name:id (~seq #:error-value errv)
+                 (~seq #:env r)
+                 (~seq #:sto s)
+                 (~seq #:matching m) . args) clauses ...+)
+     #'(define (name r s m . args)
+         (match-do #:error-value errv #:env r #:sto s m clauses ...))]))
